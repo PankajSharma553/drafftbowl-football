007f320ed3792ad8bc94974a1e9a8fce59f4341a
Pankaj Sharma <pankaj.avology@gmail.com>
2025-01-08 18:30:51 +0530

    Updated ui

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,3 +10,16 @@ $(document).ready(function () {
 
     });
 });
+
+// testimonial section 
+$(document).ready(function () {
+    $('.testimonialSlider').slick({
+        slidesToShow: 3,
+        autoplay: true,
+        arrows: true,
+        dots:false,
+        speed: 500,
+        autoplaySpeed:800,
+
+    });
+});
