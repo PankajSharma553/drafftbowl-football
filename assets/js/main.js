7e15fc377869bef16b4a3ede3efd219f5ec838d1
Pankaj Sharma <pankaj.avology@gmail.com>
2025-01-09 18:40:30 +0530

    Update ui

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,11 @@
 $(document).ready(function () {
-    $('.bannerSlider').slick({
-        // centerMode: true,
+    $('.heroSlider').slick({
         slidesToShow: 1,
         autoplay: true,
         arrows: false,
-        dots:true,
+        dots: true,
         speed: 500,
-        autoplaySpeed:80000000,
+        autoplaySpeed:800,
 
     });
 });
@@ -17,9 +16,42 @@ $(document).ready(function () {
         slidesToShow: 3,
         autoplay: true,
         arrows: false,
-        dots:true,
+        dots: true,
         speed: 500,
-        autoplaySpeed:8000000,
+        autoplaySpeed: 8000000,
 
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                },
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                },
+            },
+
+        ],
     });
 });
+
+
+// Navbar menu
+
+const navbar = document.querySelector(".header_navbar");
+const humburger = document.querySelector(".humburger");
+const close = document.querySelector(".close_menu");
+
+humburger.addEventListener("click", function () {
+    navbar.classList.add("active");
+});
+close.addEventListener("click", function () {
+    navbar.classList.remove("active");
+});
+
+
